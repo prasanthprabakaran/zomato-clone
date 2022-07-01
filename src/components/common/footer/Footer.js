cad2096cbf3ffc0039cfab63b05a41e3b5bcf33c
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-07-01 20:52:44 +0530

    dining

--- src/components/common/footer/Footer.js
+++ src/components/common/footer/Footer.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Footer.css'
+import FooterLinks from './footerComponents/FooterLinks'
+import SocialLinks from './footerComponents/SocialLinks'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-wrapper'>
+      <div className='footer'>
+        <div className='footer-heading'>
+          <div className='zomato-logo'></div>
+          <div className='heading-seperator'></div>
+          <div className='footer-regional'>
+            <button><span>India</span></button>
+            <button><span>English</span></button>
+          </div>
+        </div>
+        <div className='links-section'>
+          <FooterLinks />
+          <SocialLinks />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/diningOut/DiningOut.js
+++ src/components/diningOut/DiningOut.js
@@ -1,8 +1,118 @@
 import React from 'react'
+import Collection from '../common/collection/Collection'
+import Filter from '../common/filters/Filter'
+import ExploreSection from '../common/exploreSection/ExploreSection'
+import { dining } from '../../data/dining'
+import './diningout.css';
+
+
+
+const diningList = dining;
+const diningFilters = [
+  {
+    id:1,
+    title:"Filters"
+  },
+  {
+    id:2,
+    title:"Pro Offers"
+  },
+  {
+    id:3,
+    title:"Distance"
+  },
+  {
+    id:4,
+    title:"Online Bookings"
+  },
+  {
+    id:5,
+    title:"Outdoor Seating"
+  },
+  {
+    id:6,
+    title:"Rating: 4.0+"
+  },
+  {
+    id:7,
+    title:"Cuisines"
+  },
+  {
+    id:8,
+    title:"Cafes"
+  },
+  {
+    id:9,
+    title:"Open Now"
+  }
+]
+
+const collectionList = [
+  {
+    id: 1,
+    title: 'Newly Opened',
+    places: '16 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/b22194cb38ed18a5200b387ad8f243f0_1582015769.jpg',
+  },
+  {
+    id: 2,
+    title: 'Trending This Week',
+    places: '30 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/f60eb469dbcdadaaa48c8be4f5ba48b3_1646040541.png',
+  },
+  {
+    id: 3,
+    title: 'Best of Chennai',
+    places: '75 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/6bbbaa1ccabe15209c45f5cfddfdef6a_1620291802.jpg',
+  },
+  {
+    id: 4,
+    title: 'Best Bars & Pubs',
+    places: '31 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/420f473015c7d64962b60bb355828192_1613380120.jpg',
+  },
+  {
+    id: 5,
+    title: 'Brilliant Biriyanis',
+    places: '24 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/81dc179878a8d9e8b2ab03e4eaeab0df_1459434256.jpg',
+  },
+  {
+    id: 6,
+    title: 'Best Pizza Places in Town',
+    places: '27 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/99384005af05393bad27980abd0ef3ee_1618993177.jpg',
+  },
+  {
+    id: 7,
+    title: 'Flavours of the North',
+    places: '23 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/26ec96dfdd3e06f08fba4cce646e50e9_1618995559.jpg',
+  },
+  {
+    id: 8,
+    title: 'Terrific Thalis',
+    places: '9 Places',
+    cover: 'https://b.zmtcdn.com/data/collections/c577e0ca5b896facbba4ef70ffbba204_1618994185.jpg',
+  },
+  {
+
+  }
+];
 
 const DiningOut = () => {
   return (
-    <div>DiningOut</div>
+    <div>
+      <Collection list={collectionList}/>
+      <div className='max-width'>
+        <Filter filterList={diningFilters} />
+      </div>
+      <ExploreSection 
+      restaurants={diningList}
+      collectionName="Dine-Out Restaurants in Anna Nagar"
+      />
+    </div>
   )
 }
 
